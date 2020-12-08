291af10c9fb49f57ef2a48cff5e6f90ceac215c2
Sergey Susikov <sergey.susikov@gmail.com>
2020-12-08 12:24:10 +0200

    Day 8

--- adventofcode/Program.cs
+++ adventofcode/Program.cs
@@ -11,7 +11,7 @@ namespace adventofcode
     {
         static void Main(string[] args)
         {
-            Seven();
+            Eight();
         }
 
         static void One()
@@ -295,6 +295,118 @@ namespace adventofcode
                 return count;
             };
             Console.WriteLine(countNested(graph["shiny gold"]));
+        }
+
+        static void Eight()
+        {
+            string[] commands = File.ReadAllLines("C:\\Users\\Sergey\\source\\repos\\adventofcode\\adventofcode\\8\\input.txt");
+
+            // Part One
+            List<int> commandsRun = new List<int>();
+            int line = 0;
+            int accumulator = 0;
+            while (line >= 0 && line < commands.Length && !commandsRun.Contains(line))
+            {
+                commandsRun.Add(line);
+                string[] op = commands[line].Split(' ');
+                switch (op[0])
+                {
+                    case "acc":
+                        accumulator += int.Parse(op[1]);
+                        line++;
+                        break;
+                    case "jmp":
+                        line += int.Parse(op[1]);
+                        break;
+                    case "nop":
+                        line++;
+                        break;
+                    default:
+                        Debug.Fail("Nein!");
+                        break;
+                }
+            }
+            Console.WriteLine(accumulator);
+
+            // Part Two
+            Console.WriteLine("-----------------");
+            List<string> commandsOpsRun = new List<string>();
+            commandsRun = new List<int>();
+            line = 0;
+            List<int> accumulatorStates = new List<int>();
+            int? branchPoint = null;
+            while (line >= 0 && line < commands.Length)
+            {
+                commandsRun.Add(line);
+                string[] op = commands[line].Split(' ');
+                commandsOpsRun.Add(op[0]);
+                accumulator = accumulatorStates.Count > 0 ? accumulatorStates.Last() : 0;
+                switch (op[0])
+                {
+                    case "acc":
+                        accumulator += int.Parse(op[1]);
+                        line++;
+                        break;
+                    case "jmp":
+                        line += int.Parse(op[1]);
+                        // Look at branch point and look back at previous commands run. Attempt to change jmp to nop or nop to jmp.
+                        if (commandsRun.Contains(line))
+                        {
+                            if (branchPoint == null)
+                            {
+                                branchPoint = commandsRun.Count - 1;
+                            }
+
+                            // Rolling back to branchPoint.
+                            accumulatorStates.RemoveRange((int)branchPoint, accumulatorStates.Count - (int)branchPoint);
+                            commandsRun.RemoveRange((int)branchPoint, commandsRun.Count - (int)branchPoint);
+                            commandsOpsRun.RemoveRange((int)branchPoint, commandsOpsRun.Count - (int)branchPoint);
+
+                            // One more step back.
+                            accumulatorStates.RemoveAt(accumulatorStates.Count - 1);
+                            commandsRun.RemoveAt(commandsRun.Count - 1);
+                            commandsOpsRun.RemoveAt(commandsOpsRun.Count - 1);
+
+                            // Find first no-acc command in the log.
+                            while (commandsOpsRun.Last() == "acc")
+                            {
+                                accumulatorStates.RemoveAt(accumulatorStates.Count - 1);
+                                commandsRun.RemoveAt(commandsRun.Count - 1);
+                                commandsOpsRun.RemoveAt(commandsOpsRun.Count - 1);
+                            }
+
+                            string[] branchOp = commands[commandsRun.Last()].Split(' ');
+                            string actualCommandRun = commandsOpsRun.Last();
+
+                            // Now actually run the opposite action.
+                            if (actualCommandRun == "jmp")
+                            {
+                                line = commandsRun.Last() + 1;
+                            }
+                            else if (actualCommandRun == "nop")
+                            {
+                                line = commandsRun.Last() + int.Parse(branchOp[1]);
+                            }
+
+                            // Record all the states again.
+                            commandsOpsRun.RemoveAt(commandsOpsRun.Count - 1);
+                            commandsOpsRun.Add(actualCommandRun == "jmp" ? "nop" : "jmp");
+                            // Don't forget to update branching point!
+                            branchPoint = commandsRun.Count - 1;
+
+                            accumulator = accumulatorStates.Last();
+                        }
+                        break;
+                    case "nop":
+                        line++;
+                        break;
+                    default:
+                        Debug.Fail("Nein!");
+                        break;
+                }
+                accumulatorStates.Add(accumulator);
+            }
+            Console.WriteLine(accumulator);
         }
     }
 }
